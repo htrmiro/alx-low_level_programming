d61c34f36bca81e6d01318d53dd4b574acbe0e62
htrmiro <odegbamiaccess@gmail.com>
2022-06-08 10:59:13 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-		int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-		write("and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      	      s);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 }
