94ce06013f3a59f67089326a32f5dc41ffed1a43
htrmiro <odegbamiaccess@gmail.com>
2022-06-08 11:15:44 -0700

    e

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Description: and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
  * Return: Always 0 (Success)
  */
