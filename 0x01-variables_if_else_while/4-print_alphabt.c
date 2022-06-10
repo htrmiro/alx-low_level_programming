419f86b256d98b9779240c5638414e73ec7c7cf5
htrmiro <odegbamiaccess@gmail.com>
2022-06-10 11:40:10 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *list every letter except q & e
+ *Return: 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		
+		if (c != 'q'&& c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0):
+}
