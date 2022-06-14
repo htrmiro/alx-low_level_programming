6a080144ef01ecd1dae9867c948adbb8dd090af0
htrmiro <odegbamiaccess@gmail.com>
2022-06-13 18:07:57 -0700

    added header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,36 @@
+/*header file 0x04*/
+/*only allowed print fonction*/
+int _putchar(char);
+
+/*question 0*/
+void reset_to_98(int *n);
+
+/*question 1*/
+void swap_int(int *a, int *b);
+
+/*question 2*/
+int _strlen(char *s);
+
+/*question 3*/
+void _puts(char *str);
+
+/*question 4*/
+void print_rev(char *s);
+
+/*question 5*/
+void rev_string(char *s);
+
+/*question 6*/
+void puts2(char *str);
+
+/*question 7*/
+void puts_half(char *str);
+
+/*question 8*/
+void print_array(int *a, int n);
+
+/*question 9*/
+char *_strcpy(char *dest, char *src);
+
+/*question 10*/
+int _atoi(char *s);
