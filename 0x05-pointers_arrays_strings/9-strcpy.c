a4f96c0994a8b75d4c36c9a3f1c76ebd91334729
htrmiro <odegbamiaccess@gmail.com>
2022-06-13 21:49:46 -0700

    fixes to various task errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
+**
  *_strcpy - copies the string pointed to by src,
  *including the terminating null byte, to the
  *buffer pointed to by dest.
- *dest: destination.
- *src: source.
+ *@dest: destination.
+ *@src: source.
  *Return: the pointer to dest.
  */
 
