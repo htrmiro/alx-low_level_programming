afe66c3d84402c1d72e468a22056a9fb3698c723
htrmiro <odegbamiaccess@gmail.com>
2022-06-15 14:02:03 -0700

    variables task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+**
+ *main - main entry
+ *all single digit numbers base ten from zero
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
