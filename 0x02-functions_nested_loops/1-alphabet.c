52d4adf768b7783896f38990faa452048e3c06c9
htrmiro <odegbamiaccess@gmail.com>
2022-07-24 14:45:35 -0700

    Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
 #include <unistd.h>
 #include "main.h"
-/** main: print all letters using putchar
- *
- * a
- * z 
- * return: 0 
- */     
+/**
+ * main: print all letters using putchar
+ *a
+ *z
+ *return: 0 
+ */
 void print_alphabet(void)
 {
 	char a = 'a';
-	
 	while (a <= 'z')
 	{
 		_putchar(a);
 		a++;
 	}
-	
 	_putchar('\n');
 }
 
