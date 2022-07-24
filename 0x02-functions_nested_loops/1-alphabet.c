17d889213174b7c1fa1da4dbfde78b5d75934c36
htrmiro <odegbamiaccess@gmail.com>
2022-07-24 15:02:49 -0700

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
-#include <unistd.h>
 #include "main.h"
-/**
- * main: print all letters using putchar
+**
+ * print_alphabet: print all lowercase letters using '_putchar'
  *a
  *z
- *return: 0 
+ *return: 0
  */
 void print_alphabet(void)
 {
