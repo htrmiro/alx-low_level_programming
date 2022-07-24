cc46251690cf4f459f73834a9a8fffc4d7142615
htrmiro <odegbamiaccess@gmail.com>
2022-07-24 14:09:41 -0700

    Betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
 
-int main(){
-	write (1," _putchar\n",9);
-
-
-
+int main(void) 
+{
+	write(1, " _putchar\n", 9);
+	
 	return 0;	
 }
