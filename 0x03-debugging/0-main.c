4b4f342e7deabbc3b52e575ae8b3c65819417fec
htrmiro <odegbamiaccess@gmail.com>
2022-07-26 13:10:46 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
- *main - main
- * Get a random number and print the number
- * check positive, negative, or zero
- * Return: 0
- */
+ *  * main - main
+ *   * Return: 0
+ *    */
+
 int main(void)
 {
-	int n;
+	int i;
+	i = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	positive_or_negative(i);
 
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
 	return (0);
+
 }
